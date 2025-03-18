28b6adfacdd49770a1dc954edfa51628905d3d71
Ian Rokas <grokas@splunk.com>
2025-03-18 06:36:12 +0000

    chore(ci): workflow updates and pre-commit changes and configurations added and applied
    [batch update]

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 # File: __init__.py
-# Copyright (c) ReversingLabs, 2023
+# Copyright (c) ReversingLabs, 2023-2025
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
